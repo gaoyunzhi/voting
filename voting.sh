f87e565b594e7bdce5023721bdfb9fd9de405778
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-20 22:34:06 -0800

    commit

---
+++ voting.sh
@@ -0,0 +1,8 @@
+curl -o https://raw.githubusercontent.com/gaoyunzhi/voting/master/voting.py voting.py
+nohup python3 voting.py &
+nohup python3 voting.py &
+nohup python3 voting.py &
+nohup python3 voting.py &
+nohup python3 voting.py &
+nohup python3 voting.py &
+python3 voting.py
