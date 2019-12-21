467748de95aef9cc6ec1c69e0a1c627ccf79a70c
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-21 04:44:46 -0800

    commit

--- voting.py
+++ voting.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import requests
 import time
 import random
+import os
+import sys
+
+if sys.version_info[0] == 3:
+    pythonv = '3'
+else:
+    pythonv = ''
+
+try:
+    import requests
+except:
+    r = os.system('pip%s install -r requirements.txt' % pythonv)
+    if r != 0:
+        os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
+        os.system('python%s get-pip.py' % pythonv)
+        os.system('rm get-pip.py')
+        os.system('pip%s install -r requirements.txt' % pythonv)
+    import requests
 
 VOTING_URL = 'https://sv.ifeng.com/index.php/survey/postsurvey?sur[32486][]=127074&id=15990&act=postsurvey&surid=15990&ref=http%3A%2F%2Ffinance.ifeng.com%2Fsurvey.html&callback=postSurveyData'
 SUCCESS = '\\u6295\\u7968\\u6210\\u529f\\uff0c\\u611f\\u8c22\\u60a8\\u53c2\\u4e0e' # 投票成功，感谢您参与'
