78233b7d0c88cb845f8647dbba69e3ccca2ca265
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-21 04:36:02 -0800

    commit

--- voting.sh
+++ voting.sh
@@ -9,7 +9,7 @@ if command -v python3 &>/dev/null; then
 	nohup python3 voting.py &
 	nohup python3 voting.py &
 	nohup python3 voting.py &
-	python3 voting.py
+	python3 -W ignore voting.py
 elif command -v python &>/dev/null; then
 	command -v pip install -r requirements.txt
 	nohup python voting.py &
@@ -18,7 +18,7 @@ elif command -v python &>/dev/null; then
 	nohup python voting.py &
 	nohup python voting.py &
 	nohup python voting.py &
-	python voting.py
+	python -W ignore voting.py
 else
     echo Python 3 is required, please install it.
     echo "Windows installer: https://www.python.org/ftp/python/3.8.1/python-3.8.1-amd64.exe";
